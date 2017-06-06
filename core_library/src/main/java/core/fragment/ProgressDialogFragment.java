52316cef36567aef7ac3ee24c5f1960399e8d11a
Armando Mennini <armando.mennini@gmail.com>
2017-06-06 22:34:16 +0200

    added progress dilaog fragment

--- core_library/src/main/java/core/fragment/ProgressDialogFragment.java
+++ core_library/src/main/java/core/fragment/ProgressDialogFragment.java
@@ -5,7 +5,7 @@ import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentActivity;
 
 public class ProgressDialogFragment extends DialogFragment {
     private static final String KEY_MSG = "KEY_MSG";
@@ -23,25 +23,25 @@ public class ProgressDialogFragment extends DialogFragment {
         return pd;
     }
 
-    public void show(FragmentManager fragmentManager, int stringId) {
-        try {
-            Bundle arguments = new Bundle(1);
-            arguments.putInt(KEY_MSG, stringId);
-            setArguments(arguments);
-            if (!ProgressDialogFragment.wantToCloseDialog) {
-                show(fragmentManager, ProgressDialogFragment.class.getSimpleName());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     public void updateMessage(String msg) {
         pd.setMessage(msg);
     }
 
     public static void wantToCloseDialog(boolean wantToCloseDialog) {
         ProgressDialogFragment.wantToCloseDialog = wantToCloseDialog;
+    }
+
+    public void show(FragmentActivity context, int resId) {
+        try {
+            Bundle arguments = new Bundle(1);
+            arguments.putInt(KEY_MSG, resId);
+            setArguments(arguments);
+            if (!ProgressDialogFragment.wantToCloseDialog) {
+                show(context.getSupportFragmentManager(), ProgressDialogFragment.class.getSimpleName());
+            }
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        }
     }
 }
 
