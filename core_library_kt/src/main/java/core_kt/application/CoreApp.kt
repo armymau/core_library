4299dc9c7b73edc3596c9cee867c11e6245e049e
Armando Mennini <armando.mennini@gmail.com>
2018-09-01 13:41:08 +0200

    updated version

--- core_library_kt/src/main/java/core_kt/application/CoreApp.kt
+++ core_library_kt/src/main/java/core_kt/application/CoreApp.kt
@@ -22,13 +22,22 @@ open class CoreApp : Application() {
     var isTabletTipology: Boolean = false
 
     lateinit var mRequestQueue: RequestQueue
-    private lateinit var instance: CoreAppSingleton
 
+
+    class CoreApp private constructor(context: Context) {
+        init {
+            println("init complete")
+        }
+        companion object : SingletonHolder<CoreApp, Context>(::CoreApp)
+    }
+
+    /*
     companion object CoreAppSingleton {
         init {
             println("init complete")
         }
     }
+    */
 
     /*
     @Synchronized
@@ -50,8 +59,6 @@ open class CoreApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        instance = CoreAppSingleton
-
         initImageLoader(applicationContext)
     }
 
@@ -80,5 +87,29 @@ open class CoreApp : Application() {
                 .tasksProcessingOrder(QueueProcessingType.LIFO)
                 .build()
         ImageLoader.getInstance().init(config)
+    }
+
+    open class SingletonHolder<out T, in A>(creator: (A) -> T) {
+        private var creator: ((A) -> T)? = creator
+        @Volatile private var instance: T? = null
+
+        fun getInstance(arg: A): T {
+            val i = instance
+            if (i != null) {
+                return i
+            }
+
+            return synchronized(this) {
+                val i2 = instance
+                if (i2 != null) {
+                    i2
+                } else {
+                    val created = creator!!(arg)
+                    instance = created
+                    creator = null
+                    created
+                }
+            }
+        }
     }
 }
