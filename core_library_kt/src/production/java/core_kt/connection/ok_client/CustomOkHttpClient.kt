4570bf4f3448e44a2f8f6ad3875f721f86357a2f
Armando Mennini <armando.mennini@gmail.com>
2019-03-16 22:00:37 +0100

    added multi variant

---
+++ core_library_kt/src/production/java/core_kt/connection/ok_client/CustomOkHttpClient.kt
@@ -0,0 +1,260 @@
+package core_kt.connection.ok_client
+
+import android.util.Log
+import core_kt.utils.TAG
+import core_kt.utils.isDebug
+import core_kt.utils.isSigned
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.Response
+import java.security.SecureRandom
+import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
+import java.util.*
+import java.util.concurrent.TimeUnit
+import javax.net.ssl.KeyManager
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
+
+private val HTTP_TIMEOUT = 120 * 1000 // milliseconds
+
+fun getOkHttpClient(): OkHttpClient.Builder {
+
+    val okHttpClient = OkHttpClient.Builder()
+    okHttpClient.connectTimeout(HTTP_TIMEOUT.toLong(), TimeUnit.MILLISECONDS)
+    okHttpClient.readTimeout(HTTP_TIMEOUT.toLong(), TimeUnit.MILLISECONDS)
+    okHttpClient.writeTimeout(HTTP_TIMEOUT.toLong(), TimeUnit.MILLISECONDS)
+
+    return okHttpClient
+}
+
+fun getOkHttpsClient(): OkHttpClient.Builder {
+
+    val okHttpClient = getOkHttpClient()
+    okHttpClient.hostnameVerifier { _, _ -> true }
+
+    val x509TrustManager = object : X509TrustManager {
+        @Throws(CertificateException::class)
+        override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {
+        }
+
+        @Throws(CertificateException::class)
+        override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {
+            chain[0].checkValidity()
+        }
+
+        override fun getAcceptedIssuers(): Array<X509Certificate?> {
+            return arrayOfNulls(0)
+        }
+    }
+    val trustAllCerts = arrayOf<TrustManager>(x509TrustManager)
+
+    try {
+        val sslContext = SSLContext.getInstance("TLS")
+        sslContext.init(null as Array<KeyManager>?, trustAllCerts, SecureRandom())
+        val sslSocketFactory = sslContext.socketFactory
+        okHttpClient.sslSocketFactory(sslSocketFactory, x509TrustManager)
+    } catch (var2: Exception) {
+        var2.printStackTrace()
+    }
+
+    return okHttpClient
+}
+
+fun doGetRequest(methodName: String, url: String): String? {
+    val response: Response
+    try {
+        val request = Request.Builder()
+                .url(url)
+                .build()
+
+        response =
+                if (url.contains("https://"))
+                    getOkHttpsClient().build().newCall(request).execute()
+                else
+                    getOkHttpClient().build().newCall(request).execute()
+
+        if (!isDebug && !isSigned) {
+            Log.d(TAG, ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+            Log.d(TAG, "GET")
+            Log.e(TAG, methodName)
+            Log.d(TAG, url)
+            Log.d(TAG, "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+            Log.d(TAG, "CODE         : \n" + response.code())
+            Log.d(TAG, "RESPONSE     : \n" + response.toString())
+        }
+        return response.body()?.string()
+
+    } catch (e: Exception) {
+        e.printStackTrace()
+        return null
+    }
+}
+
+fun doPostRequest(methodName: String, url: String, formBody: RequestBody): String? {
+    val response: Response
+    try {
+        val request = Request.Builder()
+                .url(url)
+                .post(formBody)
+                .build()
+
+        response =
+                if (url.contains("https://"))
+                    getOkHttpsClient().build().newCall(request).execute()
+                else
+                    getOkHttpClient().build().newCall(request).execute()
+
+        if (!isDebug && !isSigned) {
+            Log.d(TAG, ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+            Log.d(TAG, "POST")
+            Log.e(TAG, methodName)
+            Log.d(TAG, url)
+            Log.d(TAG, "PARAMETERS   : \n$formBody")
+            Log.d(TAG, "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+            Log.d(TAG, "CODE         : \n" + response.code())
+            Log.d(TAG, "RESPONSE     : \n" + response.toString())
+        }
+        return response.body()?.string()
+
+    } catch (e: Exception) {
+        e.printStackTrace()
+        return null
+    }
+}
+
+fun doGetRequestWithHeader(methodName: String, url: String, authorization: String): String? {
+    val response: Response
+    try {
+        val request = Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("Authorization", authorization)
+                .build()
+
+        response =
+                if (url.contains("https://"))
+                    getOkHttpsClient().build().newCall(request).execute()
+                else
+                    getOkHttpClient().build().newCall(request).execute()
+
+        if (!isDebug && !isSigned) {
+            Log.d(TAG, ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+            Log.d(TAG, "GET")
+            Log.e(TAG, methodName)
+            Log.d(TAG, url)
+            Log.d(TAG, "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+            Log.d(TAG, "CODE         : \n" + response.code())
+            Log.d(TAG, "RESPONSE     : \n" + response.toString())
+        }
+        return response.body()?.string()
+
+    } catch (e: Exception) {
+        e.printStackTrace()
+        return null
+    }
+}
+
+fun doGetRequestWithMultiHeader(methodName: String, url: String, authorizations: HashMap<String, String>): String? {
+    val response: Response
+    try {
+        val request = Request.Builder().url(url).get()
+
+        val it = authorizations.entries.iterator()
+        while (it.hasNext()) {
+            val pair = it.next() as Map.Entry<*, *>
+            request.addHeader(pair.key as String, pair.value as String)
+        }
+        request.build()
+
+        response =
+                if (url.contains("https://"))
+                    getOkHttpsClient().build().newCall(request.build()).execute()
+                else
+                    getOkHttpClient().build().newCall(request.build()).execute()
+
+        if (!isDebug && !isSigned) {
+            Log.d(TAG, ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+            Log.d(TAG, "GET MULTI HEADER")
+            Log.e(TAG, methodName)
+            Log.d(TAG, url)
+            Log.d(TAG, "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+            Log.d(TAG, "CODE         : \n" + response.code())
+            Log.d(TAG, "RESPONSE     : \n" + response.toString())
+        }
+        return response.body()?.string()
+
+    } catch (e: Exception) {
+        e.printStackTrace()
+        return null
+    }
+}
+
+fun doPostRequestWithMultiHeader(methodName: String, url: String, authorizations: HashMap<String, String>, formBody: RequestBody): String? {
+    val response: Response
+    try {
+        val request = Request.Builder().url(url).post(formBody)
+
+        val it = authorizations.entries.iterator()
+        while (it.hasNext()) {
+            val pair = it.next() as Map.Entry<*, *>
+            request.addHeader(pair.key as String, pair.value as String)
+        }
+        request.build()
+
+        response =
+                if (url.contains("https://"))
+                    getOkHttpsClient().build().newCall(request.build()).execute()
+                else
+                    getOkHttpClient().build().newCall(request.build()).execute()
+
+        if (!isDebug && !isSigned) {
+            Log.d(TAG, ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+            Log.d(TAG, "POST MULTI HEADER")
+            Log.e(TAG, methodName)
+            Log.d(TAG, url)
+            Log.d(TAG, "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+            Log.d(TAG, "CODE         : \n" + response.code())
+            Log.d(TAG, "RESPONSE     : \n" + response.toString())
+        }
+        return response.body()?.string()
+
+    } catch (e: Exception) {
+        e.printStackTrace()
+        return null
+    }
+}
+
+fun doPostRequestWithMultipart(methodName: String, url: String, formBody: RequestBody): String? {
+    val response: Response
+    try {
+        val request = Request.Builder()
+                .url(url)
+                .post(formBody)
+                .build()
+
+        response =
+                if (url.contains("https://"))
+                    getOkHttpsClient().build().newCall(request).execute()
+                else
+                    getOkHttpClient().build().newCall(request).execute()
+
+        if (!isDebug && !isSigned) {
+            Log.d(TAG, ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+            Log.d(TAG, "POST WITH MULTIPART")
+            Log.e(TAG, methodName)
+            Log.d(TAG, url)
+            Log.d(TAG, "PARAMETERS   : \n$formBody")
+            Log.d(TAG, "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+            Log.d(TAG, "CODE         : \n" + response.code())
+            Log.d(TAG, "RESPONSE     : \n" + response.toString())
+        }
+        return response.body()?.string()
+
+    } catch (e: Exception) {
+        e.printStackTrace()
+        return null
+    }
+}
---
+++ core_library_kt/src/production/java/core_kt/utils/CoreConstants.kt
@@ -0,0 +1,61 @@
+package core_kt.utils
+
+import java.util.regex.Pattern
+
+const val TAG = ">>> CORE"
+const val isDebug = false
+const val isSigned = true
+
+const val HTTP_TIMEOUT = 15 * 1000    // milliseconds
+const val LONG_DELAY = 5000          // 3.5 seconds
+const val SHORT_DELAY = 2000         // 2 seconds
+
+const val NO_CONNECTION = 0
+const val MOBILE = 1
+const val WIFI = 2
+
+const val CHECK_PERMISSIONS_REQUEST_CODE = 3423
+
+const val HTTP_REQUEST_METHOD_GET = "GET"
+const val HTTP_REQUEST_METHOD_POST = "POST"
+const val HTTP_METHOD_REPONSE_CACHING_FAILED = "REPONSE_CACHING_FAILED"
+const val HTTP_METHOD_GET = 0
+const val HTTP_METHOD_POST = 1
+const val HTTP_METHOD_GET_WITH_AUTHORIZATION = 2
+const val HTTP_METHOD_POST_WITH_MULTIPART = 3
+const val HTTP_METHOD_GET_WITH_MULTI_AUTHORIZATION = 4
+const val HTTP_METHOD_POST_WITH_MULTI_AUTHORIZATION = 5
+const val HTTP_METHOD_CACHED = "CACHED RESPONSE"
+
+
+//SECURITY UTILS
+const val DELIMITER = "]"
+const val CIPHER_ALGORITHM = "AES/CBC/PKCS5Padding"
+
+//RATING POPUP UTILS
+const val INTERVAL_DAY_RATING_POPUP = 3
+const val INTERVAL_DAY_RATING_POPUP_REMIND = 7
+const val MARKET_DETAILS = "market://details?id="
+const val MARKET_LANDING_PAGE = "http://play.google.com/store/apps/details?id="
+const val RATING_PREFERENCES = "rating_preferences"
+const val RATING_DATE = "rating_date"
+const val RATING_SHOW_POPUP = "rating_showpopup"
+const val RATING_SHOW_REMIND = "rating_remind"
+
+//PATTERN REGEX
+const val PATTERN_IMAGE_REGEX = "<img[^>]+src\\s*=\\s*['\"]([^'\"]+)['\"][^>]*>"
+const val PATTERN_URL_REGEX = "\\(?\\b(http://|www[.])[-A-Za-z0-9+&@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#/%=~_()|]"
+const val PATTERN_MENTION_REGEX = "@([A-Za-z0-9_-]+)"
+const val PATTERN_HASHTAG_REGEX = "#([A-Za-z0-9_-]+)"
+const val PATTERN_NUMBER_REGEX = "^[0-9]{7,15}$"
+const val PATTERN_EMAIL_REGEX = "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" + "\\@" + "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" + "(" + "\\." + "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" + ")+"
+const val PATTERN_NAME_REGEX = "^[_A-Za-z-\\+]+(\\.[_A-Za-z-]+)*"
+const val PATTERN_PASSWORD_REGEX = "(?=.*\\d)(?=.*[a-zA-Z0-9])"
+
+val NUMBER_PATTERN = Pattern.compile(PATTERN_NUMBER_REGEX)
+val EMAIL_PATTERN = Pattern.compile(PATTERN_EMAIL_REGEX)
+val NAME_PATTERN = Pattern.compile(PATTERN_NAME_REGEX)
+val IMAGE_PATTERN = Pattern.compile(PATTERN_IMAGE_REGEX)
+val PASSWORD_PATTERN = Pattern.compile(PATTERN_PASSWORD_REGEX)
+val MENTION_PATTERN = Pattern.compile(PATTERN_MENTION_REGEX)
+val HASHTAG_PATTERN = Pattern.compile(PATTERN_HASHTAG_REGEX)
